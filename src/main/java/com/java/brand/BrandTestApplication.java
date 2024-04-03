2e11d8b81b30de28fc5b2644dd81e8ad2d6d4d5e
= <=>
2024-04-03 23:10:45 +0200

    initial commit

---
+++ src/main/java/com/java/brand/BrandTestApplication.java
@@ -0,0 +1,13 @@
+package com.java.brand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandTestApplication.class, args);
+	}
+
+}
