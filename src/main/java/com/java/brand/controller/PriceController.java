6619f3b1b98860676d65b5f574b9d8f0b5422949
= <=>
2024-04-04 00:26:31 +0200

    add openapi dependency

---
+++ src/main/java/com/java/brand/controller/PriceController.java
@@ -0,0 +1,27 @@
+package com.java.brand.controller;
+
+import com.java.brand.service.PriceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/prices")
+public class PriceController {
+    private final PriceService priceService;
+
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
+
+
+    @GetMapping("/api/prices")
+    public ResponseEntity getPrice () {
+
+
+        return null;
+    }
+}
