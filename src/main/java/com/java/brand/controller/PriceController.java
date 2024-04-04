7d8e00a1bf7b9e5002e7ff068ef51cdd7339dbdc
= <=>
2024-04-04 10:30:26 +0200

    add controller and update service with optionals

--- src/main/java/com/java/brand/controller/PriceController.java
+++ src/main/java/com/java/brand/controller/PriceController.java
@@ -1,11 +1,21 @@
 package com.java.brand.controller;
 
+import com.java.brand.model.Price;
 import com.java.brand.service.PriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @RestController
@@ -19,9 +29,21 @@ public class PriceController {
 
 
     @GetMapping("/api/prices")
-    public ResponseEntity getPrice () {
+    @Operation(summary = "Get price for a product in a specific date",
+            description = "Get price for a product in a specific date",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Price not found")
+            })
+    public ResponseEntity<Price> getPrice(
+            @Parameter(description = "Date ", required = true)
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date,
+            @Parameter(description = "Product Id", required = true)
+            @RequestParam Long productId,
+            @Parameter(description = "Brand Id", required = true)
+            @RequestParam Long brandId) {
 
-
-        return null;
+        Optional<Price> price = priceService.findActivePrice(date, productId, brandId);
+        return price.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/java/brand/service/PriceService.java
+++ src/main/java/com/java/brand/service/PriceService.java
@@ -5,7 +5,9 @@ import com.java.brand.repository.PriceRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PriceService {
@@ -15,8 +17,8 @@ public class PriceService {
         this.priceRepository = priceRepository;
     }
 
-    public List<Price> findActivePrice (LocalDateTime date, Long productId, Long brandId) {
+    public Optional<Price> findActivePrice (LocalDateTime date, Long productId, Long brandId) {
         return priceRepository.findByStartDateLessThanEqualAndEndDateGreaterThanAndProductIdAndBrandId(
-                date, productId, brandId);
+                date, productId, brandId).stream().max(Comparator.comparingInt(Price::getPriority));
     }
 }
